d4fdfd51ee321db88d36c7d699811ce6df1219d6
adityach0uhan <m4with6x@gmail.com>
2024-09-27 16:33:53 +0530

    added navbar , home page and other pages

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import './font.css'
+import Navbar from './components/Navbar.jsx'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <BrowserRouter>
+    <Navbar />
     <App />
-  </StrictMode>,
+  </BrowserRouter>,
 )
